9c639a36f9183ec99069aded871f1a51c44e25b1
799953468 <799953468@qq.com>
2020-11-02 22:59:30 +0800

    Update

--- Scripts/JD/jd_fruit.js
+++ Scripts/JD/jd_fruit.js
@@ -1249,13 +1249,13 @@ function shareCodesFormat() {
     return new Promise(async resolve => {
         // console.log(`第${$.index}个京东账号的助力码:::${jdFruitShareArr[$.index - 1]}`)
         newShareCodes = [];
-        if (jdFruitShareArr[$.index - 1]) {
-            newShareCodes = jdFruitShareArr[$.index - 1].split('@');
-        } else {
-            console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-            const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-            newShareCodes = shareCodes[tempIndex].split('@');
-        }
+        // if (jdFruitShareArr[$.index - 1]) {
+        //     newShareCodes = jdFruitShareArr[$.index - 1].split('@');
+        // } else {
+        // console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
+        const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
+        newShareCodes = shareCodes[tempIndex].split('@');
+        //}
         const readShareCodeRes = await readShareCode();
         if (readShareCodeRes && readShareCodeRes.code === 200) {
             newShareCodes = newShareCodes.concat(readShareCodeRes.data || []);
