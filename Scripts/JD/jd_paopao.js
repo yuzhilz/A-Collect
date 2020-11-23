212be16891b2307d8deb44381da7e0f9c4d6e635
799953468 <799953468@qq.com>
2020-11-23 11:59:12 +0800

    fix

--- Scripts/JD/jd_paopao.js
+++ Scripts/JD/jd_paopao.js
@@ -75,11 +75,11 @@ async function activity_taskInfo() {
 async function share() {
     console.log('开始分享');
     for (i = $.taskInfo.data.shareTaskInfo.hasSharedTimes; i < $.taskInfo.data.shareTaskInfo.shareLimit; i++) {
-        await activity_taskInfo();
         if ($.taskInfo.data.shareTaskInfo.hasSharedTimes === $.taskInfo.data.shareTaskInfo.shareLimit) {
             break;
         }
         await shareTask();
+        await activity_taskInfo();
         if ($.shareInfo) {
             console.log('任务执行：' + $.shareInfo.errMsg + ' ' + $.taskInfo.data.shareTaskInfo.hasSharedTimes + '/' + $.taskInfo.data.shareTaskInfo.shareLimit);
         }
@@ -102,11 +102,11 @@ async function activity() {
 async function product() {
     console.log('开始关注商品');
     for (i = $.taskInfo.data.taskInfo[1].finishNum; i < $.taskInfo.data.taskInfo[1].allValues.length; i++) {
-        await activity_taskInfo();
         if ($.taskInfo.data.taskInfo[1].allValues.length === $.taskInfo.data.taskInfo[1].finishNum) {
             break;
         }
         await productForactivity(i);
+        await activity_taskInfo();
         console.log($.taskInfo.data.taskInfo[1].allValues[i].id + ' ' + $.taskInfo.data.taskInfo[1].finishNum + '/' + $.taskInfo.data.taskInfo[1].allValues.length);
         if ($.productInfo) {
             console.log($.productInfo.errMsg);
