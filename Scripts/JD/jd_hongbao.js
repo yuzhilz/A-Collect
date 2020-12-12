d495ba634b85d858091777a50b0b9a2ee6d8d01a
799953468 <799953468@qq.com>
2020-12-12 10:43:55 +0800

    Update

--- Scripts/JD/jd_hongbao.js
+++ Scripts/JD/jd_hongbao.js
@@ -45,7 +45,7 @@ if ($.isNode()) {
     cookiesArr.reverse();
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-$.newShareCodes = [];
+$.newShareCodes = ['P04z54XCjVUnIaW5m9cZ2esjHVDl4-6ktbt2eQ'];
 !(async() => {
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', { "open-url": "https://bean.m.jd.com/" });
