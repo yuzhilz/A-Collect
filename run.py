a8693dcf726675f9ba2a148692d791aa417bc6aa
U188 <54225629+U188@users.noreply.github.com>
2021-03-06 22:18:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -92,6 +92,7 @@ def p_main(user,pass1,invitecode):
                 telegram_bot("p++", '邀请成功！')
                 driver.quit()
             else:
+                telegram_bot("梯子", '邀请成功！')
                 print('邀请失败！')
 def telegram_bot(title, content):
     print("\n")
